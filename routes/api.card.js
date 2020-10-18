d2a4fe1568a2a85575c558921c24869b4ccc0646
Ailine <ailine.soto@outlook.fr>
2020-10-18 13:59:30 +0200

    route card

--- routes/api.card.js
+++ routes/api.card.js
@@ -3,6 +3,15 @@ const router = new express.Router();
 const UserModel = require("./../models/User");
 const uploader = require("./../config/cloudinary");
 
+router.get("/:id", async (req, res, next) => {
+    try {
+        const card = await (await UserModel.findById(req.params.id));
+        res.json(card);
+    } catch (err) {
+        next(err);
+    }
+});
+
 router.patch("/user/:id", uploader.array("card"), async (req, res, next) => {
     const updatedUser = await UserModel.findByIdAndUpdate(req.params.id, {
         "card.recto": req.files[0].path,
